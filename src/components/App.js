ceb8e8f7b7d7349e2f6a6640aa5a7f306b5880df
Ashfaq Farleen <139785309+devmaniac1@users.noreply.github.com>
2024-07-10 15:42:43 +0000

    Load Quiz and useReducer Usage

---
+++ src/components/App.js
@@ -0,0 +1,56 @@
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+import Question from "./Question";
+
+const initialState = {
+  questions: [],
+  status: "loading", //"loading","error","ready","active","finished"
+  index: 0,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataRecieved":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    case "start":
+      return { ...state, status: "active" };
+    default:
+      return new Error("Error");
+  }
+}
+
+export default function App() {
+  const [{ questions, status, index }, dispatch] = useReducer(
+    reducer,
+    initialState,
+  );
+
+  const numQuestions = questions.length;
+
+  useEffect(() => {
+    fetch("https://kljmm8-8000.csb.app/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataRecieved", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && <Question question={questions[index]} />}
+      </Main>
+    </div>
+  );
+}
